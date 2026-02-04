6be46e8cfff3fb25f3366c39c61334dc35c667d3
BTD1GHOST <124221535+BTD1GHOST@users.noreply.github.com>
2026-02-04 03:36:41 -0500

    Integrate Firebase app and authentication setup
    Added Firebase initialization and configuration.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// Firebase config will go here
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  // PASTE YOUR CONFIG HERE
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
